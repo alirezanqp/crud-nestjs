e8f861aeef88d0955af0db1563ff8247503c0b4b
alirezanqp <alirezanqp@gmail.com>
2022-09-10 18:09:58 +0430

    📝 feat: Add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog')
+    .setDescription('simple blog api document')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(6000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
